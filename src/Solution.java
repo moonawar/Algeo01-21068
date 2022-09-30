ab146c79d20e9da739363eafe1756bd0a8435472
louis <louiscaesapdf@gmail.com>
2022-09-30 08:43:23 +0700

    menambahkan displaySolution

--- src/Solution.java
+++ src/Solution.java
@@ -32,4 +32,15 @@ public class Solution {
     public void setState(Boolean s) {
         this.state = s;
     }
+
+    public void displaySolution(Solution sol, Matrix m) {
+        if (sol.getState()) {
+            for (int j = 0; j <= m.getLastIdxCol()-1; j++) {
+                System.out.println("x" + (j + 1) + " = " + sol.getVar(j));
+            }
+
+        } else {
+            System.out.println("SPL tersebut tidak miliki solusi");
+        }
+    }
 }
