b4fcbba2491631af69638f2bba7bdc296d57f0d2
louis <louiscaesapdf@gmail.com>
2022-10-03 08:32:11 +0700

    revisi splcrammer

--- src/SPLCrammer.java
+++ src/SPLCrammer.java
@@ -1,28 +1,51 @@
 public class SPLCrammer {
-    public static void cramer(final Matrix mA, final Matrix mB) {
-        if (DetCofactor.determinanCofactor(mA) == 0.0f || mA.rowEff != mA.colEff) {
-            System.out.println("\nDeterminan Matriks inputnya 0 atau matriks bukan persegi, sehingga matriks tidak dapat diselesaikan dengan metode Cramer");
-        }
-        else {
-            System.out.println("\nSolusi SPL dengan menggunakan metode Cramer:");
-            for (int i = 0; i <= mA.getLastIdxCol(); i++) {
-                System.out.println("x" + (i + 1) + " = " + DetCofactor.determinanCofactor(subsCramer(mA, mB, i)) / DetCofactor.determinanCofactor(mA));
+    public static Matrix swapCol(Matrix mB, int kolom, Matrix mA) {
+        int i, j;
+        Matrix temp = mA.copyMatrix();
+
+        for (i = 0; i <= mA.getLastIdxRow(); ++i) {
+            for (j = 0; j <= mA.getLastIdxCol(); ++j) {
+                if (j == kolom) {
+                    temp.setElmt(i, j, mB.getElmt(i, 0));
+                } else {
+                    temp.setElmt(i, j, mA.getElmt(i, j));
+                }
             }
         }
+
+        return temp;
     }
-    
-    public static Matrix subsCramer(final Matrix mA, final Matrix mB, final int n) {
-        final Matrix mOut = new Matrix(mA.rowEff, mA.rowEff);
-        for (int i = 0; i <= mA.getLastIdxRow(); i++) {
-            for (int j = 0; j <= mA.getLastIdxCol(); j++) {
-                if (j != n) {
-                    mOut.setElmt(i, j, mA.getElmt(i, j));
-                }
-                else {
-                    mOut.setElmt(i, n, mB.getElmt(i, 0));
-                }
+
+    public static Solution crammerSPL(Matrix m) {
+        Matrix mA, mB, temp, tempMA;
+        int i, j;
+        float determinan, determinanAwal;
+        boolean state = true;
+        String[] listVarMat = new String[m.getLastIdxCol()];
+        float[] hasil = new float[m.getLastIdxCol()];
+        Solution sol = new Solution(hasil, listVarMat, state);
+        // determinan awal di index m.getLastIdxCol()
+
+        mA = Converter.augmentedToMatrix(m, true);
+        mB = Converter.augmentedToMatrix(m, false);
+
+        tempMA = mA.copyMatrix();
+        determinanAwal = DetReduction.determinanGauss(tempMA);
+        
+        if (determinanAwal == 0) {
+            sol.setState(false);
+            return sol;
+        } else {
+            // cari determinan lainnya
+            for (j = 0; j <= m.getLastIdxCol()-1; ++j) {
+                temp = swapCol(mB, j, mA);
+                determinan = DetReduction.determinanGauss(temp);
+                determinan /= determinanAwal;
+                sol.setHasil(j, determinan);
+                sol.setVar(j, String.valueOf(determinan));
             }
         }
-        return mOut;
+
+        return sol;
     }
 }
